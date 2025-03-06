4a0224d8292b6c666675739f197913bfe61ef0eb
Otávio Rodrigo Pereira Brignoni <otavio.r01@aluno.ifsc.edu.br>
2025-03-06 17:05:37 -0300

    Add project files.

---
+++ Calculadora.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace Calculadora.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora 2025");
+
+            Console.Write("Digite o primeiro número -> ");
+            int primeiroNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo número -> ");
+            int segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = primeiroNumero + segundoNumero;
+            Console.WriteLine("Resultado: " + resultado);
+
+            Console.ReadLine();
+        }
+    }
+}
