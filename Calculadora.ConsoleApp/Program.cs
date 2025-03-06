613594c8e54a5883308088c1c85f3880875d9a1e
Otávio Rodrigo Pereira Brignoni <otavio.r01@aluno.ifsc.edu.br>
2025-03-06 17:31:21 -0300

    Adicionada funcionalidade de subtração

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -6,17 +6,39 @@ namespace Calculadora.ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("----------------");
             Console.WriteLine("Calculadora 2025");
+            Console.WriteLine("----------------");
+
+            Console.WriteLine("1 - Somar");
+            Console.WriteLine("2 - Subtrair");
+            Console.WriteLine("S - Sair");
+
+            Console.Write("Escolha uma opção -> ");
+            string opcao = Console.ReadLine().ToUpper();
+            if (opcao == "S") {
+                return;
+            }
 
             Console.Write("Digite o primeiro número -> ");
             int primeiroNumero = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Digite o segundo número -> ");
             int segundoNumero = Convert.ToInt32(Console.ReadLine());
+            
+            int resultado = 0;
+            if (opcao == "1") 
+            { 
+                resultado = primeiroNumero + segundoNumero;
+            }
+            else
+            {
+                resultado = primeiroNumero - segundoNumero;
+            }
 
-            int resultado = primeiroNumero + segundoNumero;
             Console.WriteLine("Resultado: " + resultado);
-
+            
+            Console.WriteLine("Pressione enter para sair");
             Console.ReadLine();
         }
     }
